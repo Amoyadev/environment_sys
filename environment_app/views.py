77561d8c267bdcca9995e5012c08a693aa489857
tariq628 <tariqkaleri628@gmail.com>
2023-03-02 00:16:42 +0500

    Code rendered

--- environment_app/views.py
+++ environment_app/views.py
@@ -17,37 +17,30 @@ def plataformas(request):
     return render(request, 'all_ambientes.html', context)
   
 def detalles(request, id_plataforma):
-<<<<<<< HEAD
     # myplatform = Plataforma.objects.select_related('sw_set').all()
     myplatform = Plataforma.objects.get(id_plataforma=id_plataforma)
 
     try:
         sw = myplatform.sw_set.all()  # Here is SW
         ip = sw.first().ip_set.all()  # Here is ip
-    except Exception:
+        # print(union_sw_ip)
+        # union_sw_ip = sw.union(ip, all=True)
+    except Exception as e:
+        print(e)
         sw = ''
-        ip = ''
 
-    print(sw)
-    print(ip)
+    # print(sw)
+    # print(ip)
 
     template = loader.get_template('detalles.html')
     context = {
         'myplatform': myplatform,
         'sw': sw,
-        'ip': ip,
+        'ip': ip
 
     }
     return HttpResponse(template.render(context, request))
 
-=======
-  myplatform = Plataforma.objects.get(id_plataforma=id_plataforma)
-  template = loader.get_template('detalles.html')
-  context = {
-    'myplatform': myplatform,
-  }
-  return HttpResponse(template.render(context, request))
->>>>>>> 242997a0828e67b08c97fcff62ec237eaab00b3a
 
 def main(request):
   template = loader.get_template('main.html')
